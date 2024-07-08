4ab35d755da119e7772c946dc492ad506bc711d3
ismaail <contact@ismaail.com>
2024-07-08 03:22:55 +0100

    Add Product Resource Pages

---
+++ app/Filament/Resources/ProductResource/Pages/CreatePost.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Pages;
+
+use App\Filament\Resources\ProductResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePost extends CreateRecord
+{
+    protected static string $resource = ProductResource::class;
+}
